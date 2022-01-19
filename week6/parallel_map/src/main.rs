1913b2a909ba6359becd4e19601db5903c473104
sponge <wangqiim@163.com>
2022-01-18 23:04:29 -0800

    week6: Sharing Data by Communicating

--- week6/parallel_map/src/main.rs
+++ week6/parallel_map/src/main.rs
@@ -7,8 +7,37 @@ where
     T: Send + 'static,
     U: Send + 'static + Default,
 {
-    let mut output_vec: Vec<U> = Vec::with_capacity(input_vec.len());
     // TODO: implement parallel map!
+    let input_num = input_vec.len();
+    let mut output_vec: Vec<U> = Vec::with_capacity(input_num);
+    output_vec.resize_with(input_num, Default::default);
+
+    let mut threads = Vec::new();
+    let (input_sender, input_receiver) = crossbeam_channel::unbounded(); // main thread --(input index) --> work thread
+    let (output_sender, output_receiver) = crossbeam_channel::unbounded(); // work thread --(output data) --> main thread
+    for _ in 0..num_threads {
+        let input_receiver = input_receiver.clone();
+        let output_sender = output_sender.clone();
+        threads.push(thread::spawn(move || {
+            while let Ok((input_index, input_val)) = input_receiver.recv() {
+                output_sender.send((input_index, f(input_val))).expect("Tried writing to channel, but there are no receivers!");
+            }
+        }));
+    }
+    for i in 0..input_num {
+        input_sender.send((input_num - i - 1, input_vec.pop().unwrap())).expect("Tried writing to channel, but there are no receivers!");
+    }
+    drop(input_sender);
+    
+    for thread in threads {
+        thread.join().expect("Panic occurred in thread");
+    }
+    drop(output_sender);
+    
+    while let Ok((output_index, output_val)) = output_receiver.recv() {
+        output_vec[output_index] = output_val;
+    }
+    
     output_vec
 }
 
